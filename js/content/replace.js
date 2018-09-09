405526c6d1c5bc8ec768ac0d800f08ee4ca46042
Mori <me@morine.net>
2018-09-08 19:24:42 -0700

    Add Options page and Bootstrap files

---
+++ js/content/replace.js
@@ -0,0 +1,12 @@
+(function() {
+    if (window.hasRun) {
+        return;
+    }
+    window.hasRun = true;
+
+    browser.runtime.onMessage.addListener((message) => {
+        if (message.command === "") {
+            
+        }
+    });
+})();
